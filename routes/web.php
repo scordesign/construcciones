ad228db72da807342f9e6967187fb272198d28ef
Seb1995 <99344424+scordesign@users.noreply.github.com>
2022-12-27 23:36:42 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::resource('users', UserController::class);
